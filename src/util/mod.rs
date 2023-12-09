51cd5add2382662181cf931d58d6ebdc89bf1ce1
kmoon2437 <kmoon2437@gmail.com>
2023-12-10 03:56:45 +0900

    보조 함수에서 mix_samples와 lerp 등을 별도 파일로 분리+Result<T,E>형의 iter에서 Result를 unwrap 없이 벗기는(?) 함수 추가

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,7 +3,9 @@
  */
 
 pub mod midi;
+pub mod synth;
 
+// 배열 비교
 pub fn compare_array<T: std::cmp::PartialEq>(arr1: &[T],arr2: &[T]) -> bool{
     if arr1.len() != arr2.len() {
         return false;
@@ -18,28 +20,15 @@ pub fn compare_array<T: std::cmp::PartialEq>(arr1: &[T],arr2: &[T]) -> bool{
     return true;
 }
 
-pub fn mix_two_samples(s1:f64,s2:f64) -> f64{
-    if s1 > 0.0 && s2 > 0.0 {
-        return (s1+s2)-(s1*s2);
-    }else if s1 < 0.0 && s2 < 0.0 {
-        return (s1+s2)+(s1*s2);
-    }else{
-        return s1+s2;
+// Result<T,E> 형의 iterator에서
+// unwrap() 없이 Result를 벗김
+pub fn unwrap_result_iter<T,E,I:std::iter::Iterator<Item = Result<T,E>>>(iter:I) -> Result<Vec<T>,E>{
+    let mut vec:Vec<T> = vec![];
+    for child in iter {
+        match child {
+            Ok(a) => vec.push(a),
+            Err(e) => return Err(e)
+        }
     }
-}
-
-pub fn mix_samples(samples:&[f64]) -> f64{
-    let mut result:f64 = 0.0;
-    for sample in samples {
-        result = mix_two_samples(result,*sample);
-    }
-    return result;
-}
-
-// 선형 보간
-// 점 A(v1),B(v2) 에 대하여
-// 선분 AB를 t : 1 - t (0 <= t <= 1) 로 내분하는 지점을 반환
-pub fn lerp(v1:f64,v2:f64,mut t:f64) -> f64{
-    t = t.max(0.0).min(1.0);
-    return (1.0 - t) * v1 + t * v2;
+    return Ok(vec);
 }
---
+++ src/util/synth.rs
@@ -0,0 +1,31 @@
+/**
+ * 음성 합성 관련 보조 함수
+ */
+
+// 샘플 2개를 mix 한다
+pub fn mix_two_samples(s1:f64,s2:f64) -> f64{
+    if s1 > 0.0 && s2 > 0.0 {
+        return (s1+s2)-(s1*s2);
+    }else if s1 < 0.0 && s2 < 0.0 {
+        return (s1+s2)+(s1*s2);
+    }else{
+        return s1+s2;
+    }
+}
+
+// 샘플 여러 개를 mix 한다
+pub fn mix_samples(samples:&[f64]) -> f64{
+    let mut result:f64 = 0.0;
+    for sample in samples {
+        result = mix_two_samples(result,*sample);
+    }
+    return result;
+}
+
+// 선형 보간
+// 점 A(v1),B(v2) 에 대하여
+// 선분 AB를 t : 1 - t (0 <= t <= 1) 로 내분하는 지점을 반환
+pub fn lerp(v1:f64,v2:f64,mut t:f64) -> f64{
+    t = t.max(0.0).min(1.0);
+    return (1.0 - t) * v1 + t * v2;
+}
