2b3c7e2a29282e272511b105eb2c21a01531cc4c
kmoon2437 <kmoon2437@gmail.com>
2023-12-11 01:11:51 +0900

    unwrap_result_iter이 vec을 그대로 반환하던 부분을 새로운 iterator를 생성해 반환하도록 수정

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -20,9 +20,31 @@ pub fn compare_array<T: std::cmp::PartialEq>(arr1: &[T],arr2: &[T]) -> bool{
     return true;
 }
 
+// vec의 원소를 하나하나 지워가면서 반복
+pub struct Iter<T>{
+    vec:Vec<T>
+}
+
+impl<T> Iter<T>{
+    pub fn new(vec:Vec<T>) -> Self{
+        return Self{ vec:vec };
+    }
+}
+
+impl<T> std::iter::Iterator for Iter<T>{
+    type Item = T;
+    fn next(&mut self) -> Option<T>{
+        if self.vec.len() == 0 {
+            return None;
+        }else{
+            return Some(self.vec.remove(0));
+        }
+    }
+}
+
 // Result<T,E> 형의 iterator에서
 // unwrap() 없이 Result를 벗김
-pub fn unwrap_result_iter<T,E,I:std::iter::Iterator<Item = Result<T,E>>>(iter:I) -> Result<Vec<T>,E>{
+pub fn unwrap_result_iter<T,E,I:std::iter::Iterator<Item = Result<T,E>>>(iter:I) -> Result<Iter<T>,E>{
     let mut vec:Vec<T> = vec![];
     for child in iter {
         match child {
@@ -30,5 +52,5 @@ pub fn unwrap_result_iter<T,E,I:std::iter::Iterator<Item = Result<T,E>>>(iter:I)
             Err(e) => return Err(e)
         }
     }
-    return Ok(vec);
+    return Ok(Iter{ vec:vec });
 }
