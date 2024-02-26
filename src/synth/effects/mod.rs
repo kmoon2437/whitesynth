4fee87387967dc05ab76b0c93a276df04a915c45
kmoon2437 <kmoon2437@gmail.com>
2024-02-26 18:24:00 +0900

    effect 관련 trait 추가

--- src/synth/effects/mod.rs
+++ src/synth/effects/mod.rs
@@ -11,3 +11,41 @@ pub mod ring_buffer;
 pub mod pitch_bend;
 pub mod reverb;
 pub mod vibrato;
+
+/**
+ * 각종 trait 정의
+ */
+
+pub trait MonoEffect {
+    fn process(&mut self, input: f64) -> f64;
+}
+
+pub trait StereoEffect {
+    fn process(&mut self, input_l: f64, input_r: f64) -> (f64, f64);
+}
+
+/**
+ * mono 이펙트 2가지를 stereo로 처리할 수 있도록 함
+ */
+pub struct StereoMonoEffect<L: MonoEffect, R: MonoEffect> {
+    left_processor: L,
+    right_processor: R
+}
+
+impl<L: MonoEffect, R: MonoEffect> StereoMonoEffect<L, R> {
+    pub fn new(left_processor: L, right_processor: R) -> Self {
+        return Self {
+            left_processor: left_processor,
+            right_processor: right_processor
+        };
+    }
+}
+
+impl<L: MonoEffect, R: MonoEffect> StereoEffect for StereoMonoEffect<L, R> {
+    fn process(&mut self, input_l: f64, input_r: f64) -> (f64, f64) {
+        return (
+            self.left_processor.process(input_l),
+            self.right_processor.process(input_r)
+        );
+    }
+}
